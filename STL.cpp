372d7667804c60c076f6b39556bf9a232903eff1
VanshDivyanshu <109081574+VanshDivyanshu@users.noreply.github.com>
2024-08-12 00:15:47 +0530

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -323,7 +323,49 @@ void explainUnorderedMap() {        // UNIQUE
     // same as set and unordered_set difference
 }
 
+bool comp(pair <int,int> p1, pair <int,int> p2) {
+    if(p1.second < p2.second) return true;
+    if(p1.second > p2.second) return false;
+    // They are same 
 
+    if(p1.first > p2.first) return true;
+    else return false;
+}
+
+void explainExtra() {
+
+    sort(a, a + n);
+    sort(v.begin(), v.end());
+
+    sort(a+2, a+4);
+
+    sort(a, a+n, greater<int>);
+
+    pair <int,int> a[] = {{1, 2}, {2, 1}, {4, 1}};
+
+    // sort it accordingly to second element 
+    // if second element is same, then sort it accordingly to first element but in descending
+
+    sort(a, a+n, comp);
+
+    // {4,1}, (2,1), {1,2}
+
+    int num = 7;
+    int cnt = _builtin_popcount();
+
+    long long num = 16578578687;
+    int cnt = _builtin_popcountll();
+
+
+    string s = "123";
+    sort(s.begin(), s.end());
+
+    do(
+        cout << s << endl;
+    ) while(next_permutation(s.begin(), s.end()));
+
+    int maxi = *max_element(a, a+n); 
+}
 
 int main() {
     print();
